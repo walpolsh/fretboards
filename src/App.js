90574a95ba8270fa2cda46ba38bb157be07f9969
Paul Walsh <twentytwentyk@live.com>
2018-11-21 11:19:56 -0500

    single string can change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Chromatic} from './constants/scales'
+import {permute} from './constants/helpers'
 import String from './components/String'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      note: Chromatic
+    }
+    this.changeNote = this.changeNote.bind(this)
+  }
+  changeNote(event) {
+    let keys = Chromatic;
+    const e = event.target.value
+    this.setState({
+      note: permute(keys, e),
+    })
+  }
   render() {
+    let note = this.state.note
     return (
       <div className="App">
-          <String Chromatic={Chromatic}/>
+          <String changeNote={this.changeNote} Chromatic={note}/>
+          <String changeNote={this.changeNote} Chromatic={note}/>
       </div>
     );
   }
--- src/components/String.js
+++ src/components/String.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 const String = (props) => {
-  let {Chromatic} = props
+  let {Chromatic, changeNote} = props
   Chromatic = Chromatic.concat(Chromatic).concat(Chromatic[0])
   return (
     <div className="stringContainer">
+      <select  onChange={changeNote}>
+        <option id='keySig' value='0'>C</option>
+        <option value='1'>Db</option>
+        <option value='2'>D</option>
+        <option value='3'>Eb</option>
+        <option value='4'>E</option>
+        <option value='5'>F</option>
+        <option value='6'>Gb</option>
+        <option value='7'>G</option>
+        <option value='8'>Ab</option>
+        <option value='9'>A</option>
+        <option value='10'>Bb</option>
+        <option value='11'>B</option>
+      </select>        
       {
-        Chromatic.map(x => <div >{x}</div>)
+          Chromatic.map(x => <div >{x}</div>)
       }
     </div>
   )

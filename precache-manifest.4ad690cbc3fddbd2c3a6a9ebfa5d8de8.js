f21189374616ea3fad08ce8e7ee819e94cb9c9a4
Paul Walsh <twentytwentyk@live.com>
2018-11-23 12:44:58 -0500

    Updates

---
+++ precache-manifest.4ad690cbc3fddbd2c3a6a9ebfa5d8de8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e78304a0bace54d50448bba14a9a8865",
+    "url": "/fretboards/static/media/header.e78304a0.png"
+  },
+  {
+    "revision": "a64cd585251e730c84bb",
+    "url": "/fretboards/static/js/runtime~main.a64cd585.js"
+  },
+  {
+    "revision": "44e2324e64b7607d96cb",
+    "url": "/fretboards/static/js/main.44e2324e.chunk.js"
+  },
+  {
+    "revision": "d369b635e2cc9a46fe93",
+    "url": "/fretboards/static/js/1.d369b635.chunk.js"
+  },
+  {
+    "revision": "44e2324e64b7607d96cb",
+    "url": "/fretboards/static/css/main.e1c36dff.chunk.css"
+  },
+  {
+    "revision": "86f983723a7a232951852c1127c982ec",
+    "url": "/fretboards/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcbc9460f35709383418ced129f323fe.js"
+  "/fretboards/precache-manifest.4ad690cbc3fddbd2c3a6a9ebfa5d8de8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/fretboards/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

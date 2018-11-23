6162d0cf4dc4c9851cfe57453430378237c29d50
Paul Walsh <twentytwentyk@live.com>
2018-11-23 12:30:39 -0500

    Updates

---
+++ precache-manifest.dcbc9460f35709383418ced129f323fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e78304a0bace54d50448bba14a9a8865",
+    "url": "/static/media/header.e78304a0.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "223cd07c0b465d8d5c5c",
+    "url": "/static/js/main.223cd07c.chunk.js"
+  },
+  {
+    "revision": "d369b635e2cc9a46fe93",
+    "url": "/static/js/1.d369b635.chunk.js"
+  },
+  {
+    "revision": "223cd07c0b465d8d5c5c",
+    "url": "/static/css/main.e1c36dff.chunk.css"
+  },
+  {
+    "revision": "15c5a3d10b05bdf666d9b530de1f376e",
+    "url": "/index.html"
+  }
+];

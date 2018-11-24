108e7efbbe8e1ff98f8c053582cc0bd6b3f5b22a
Paul Walsh <twentytwentyk@live.com>
2018-11-24 00:31:28 -0500

    class conditionals the messy way just to get it working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import banner from './header.png'
 import String from './components/String'
 import Header from './components/Header'
 import { Chromatic, Major, MelodicMinor, HarmonicMinor, HarmonicMajor, HungarianMajor, HungarianMinor, NeapolitanMinor, NeapolitanMajor, Symmetrical, Pentatonic, Kumoi, Hirojoshi } from './constants/scales';
@@ -12,12 +13,35 @@ class App extends Component {
       root: Chromatic,
       scale: Major,
       onOff: '0',
+      first: true,
+      second: true,
+      third: true,
+      fourth: true,
+      fifth: true,
+      sixth: true,
+      seventh: true,
+      eighth: true,
     }
 
     this.changeScale = this.changeScale.bind(this)
     this.changeKey = this.changeKey.bind(this)
     this.handleSwitch = this.handleSwitch.bind(this)
     this.handleHighlight = this.handleHighlight.bind(this)
+    this.handleHighlight2 = this.handleHighlight2.bind(this)
+    this.handleHighlight3 = this.handleHighlight3.bind(this)
+    this.handleHighlight4 = this.handleHighlight4.bind(this)
+    this.handleHighlight5 = this.handleHighlight5.bind(this)
+    this.handleHighlight6 = this.handleHighlight6.bind(this)
+    this.handleHighlight7 = this.handleHighlight7.bind(this)
+    this.handleHighlight8 = this.handleHighlight8.bind(this)
+
+  }
+
+  componentWillMount() {
+    this.setState({
+      list: ["first", "second", "third"],
+      listChecked: [{ first: false }, { second: false }, { third: false }]
+    });
   }
 
   changeKey(event) {
@@ -43,10 +67,56 @@ class App extends Component {
     })
   }
 
-  handleHighlight(e) {
-
+  handleHighlight() {
+    this.setState({
+      first: !this.state.first
+    })
   }
 
+  handleHighlight2() {
+    this.setState({
+      second: !this.state.second
+    })
+  }
+
+  handleHighlight3() {
+    this.setState({
+      third: !this.state.third
+    })
+  }
+
+
+  handleHighlight4() {
+    this.setState({
+      fourth: !this.state.fourth
+    })
+  }
+
+
+  handleHighlight5() {
+    this.setState({
+      fifth: !this.state.fifth
+    })
+  }
+
+
+  handleHighlight6() {
+    this.setState({
+      sixth: !this.state.sixth
+    })
+  }
+
+  handleHighlight7() {
+    this.setState({
+      seventh: !this.state.seventh
+    })
+  }
+
+  handleHighlight8() {
+    this.setState({
+      eighth: !this.state.eighth
+    })
+  }
   
   render() {
     let scale = this.state.scale
@@ -69,10 +139,10 @@ class App extends Component {
             onOff={this.state.onOff}
           />
         </div>          
-        <div >
-
+        <div style={{paddingTop: '10vh'}} className='wrapper'>
+        <div className='headerBox'>
+          <img alt='banner' src={banner} style={{width: '300px', height: '50px'}}></img>
         </div>
-        <div style={{paddingTop: '20vh'}} className='wrapper'>
         {
           scale.map((mode, i) => 
             {
@@ -94,6 +164,22 @@ class App extends Component {
                     root={this.state.root}
                     mode={mode}
                     onOff={onOff}
+                    handleHighlight={this.handleHighlight}
+                    handleHighlight2={this.handleHighlight2}
+                    handleHighlight3={this.handleHighlight3}
+                    handleHighlight4={this.handleHighlight4}
+                    handleHighlight5={this.handleHighlight5}
+                    handleHighlight6={this.handleHighlight6}
+                    handleHighlight7={this.handleHighlight7}
+                    handleHighlight8={this.handleHighlight8}
+                    first={this.state.first ? 'first' : 'hide'}  
+                    second={this.state.second ? 'second' : 'hide'}  
+                    third={this.state.third ? 'third' : 'hide'}  
+                    fourth={this.state.fourth ? 'fourth' : 'hide'}  
+                    fifth={this.state.fifth ? 'fifth' : 'hide'}  
+                    sixth={this.state.sixth ? 'sixth' : 'hide'}  
+                    seventh={this.state.seventh ? 'seventh' : 'hide'}  
+                    eighth={this.state.eighth ? 'eighth' : 'hide'}  
                   />
                   <String
                     stringName='B'
@@ -102,6 +188,23 @@ class App extends Component {
                     root={this.state.root}
                     mode={mode}
                     onOff={onOff}
+                    handleHighlight={this.handleHighlight}
+                    handleHighlight2={this.handleHighlight2}
+                    handleHighlight3={this.handleHighlight3}
+                    handleHighlight4={this.handleHighlight4}
+                    handleHighlight5={this.handleHighlight5}
+                    handleHighlight6={this.handleHighlight6}
+                    handleHighlight7={this.handleHighlight7}
+                    handleHighlight8={this.handleHighlight8}
+                    first={this.state.first ? 'first' : 'hide'}  
+                    second={this.state.second ? 'second' : 'hide'}  
+                    third={this.state.third ? 'third' : 'hide'}  
+                    fourth={this.state.fourth ? 'fourth' : 'hide'}  
+                    fifth={this.state.fifth ? 'fifth' : 'hide'}  
+                    sixth={this.state.sixth ? 'sixth' : 'hide'}  
+                    seventh={this.state.seventh ? 'seventh' : 'hide'}  
+                    eighth={this.state.eighth ? 'eighth' : 'hide'}  
+
                   />
                   <String
                     stringName='G' 
@@ -110,6 +213,24 @@ class App extends Component {
                     root={this.state.root}
                     mode={mode}
                     onOff={onOff}
+                    handleHighlight={this.handleHighlight}
+                    handleHighlight2={this.handleHighlight2}
+                    handleHighlight3={this.handleHighlight3}
+                    handleHighlight4={this.handleHighlight4}
+                    handleHighlight5={this.handleHighlight5}
+                    handleHighlight6={this.handleHighlight6}
+                    handleHighlight7={this.handleHighlight7}
+                    handleHighlight8={this.handleHighlight8}
+                    first={this.state.first ? 'first' : 'hide'}  
+                    second={this.state.second ? 'second' : 'hide'}  
+                    third={this.state.third ? 'third' : 'hide'}  
+                    fourth={this.state.fourth ? 'fourth' : 'hide'}  
+                    fifth={this.state.fifth ? 'fifth' : 'hide'}  
+                    sixth={this.state.sixth ? 'sixth' : 'hide'}  
+                    seventh={this.state.seventh ? 'seventh' : 'hide'}  
+                    eighth={this.state.eighth ? 'eighth' : 'hide'}  
+
+
                   />
                   <String
                     stringName='D' 
@@ -118,6 +239,24 @@ class App extends Component {
                     root={this.state.root}
                     mode={mode}
                     onOff={onOff}
+                    handleHighlight={this.handleHighlight}
+                    handleHighlight2={this.handleHighlight2}
+                    handleHighlight3={this.handleHighlight3}
+                    handleHighlight4={this.handleHighlight4}
+                    handleHighlight5={this.handleHighlight5}
+                    handleHighlight6={this.handleHighlight6}
+                    handleHighlight7={this.handleHighlight7}
+                    handleHighlight8={this.handleHighlight8}
+                    first={this.state.first ? 'first' : 'hide'}  
+                    second={this.state.second ? 'second' : 'hide'}  
+                    third={this.state.third ? 'third' : 'hide'}  
+                    fourth={this.state.fourth ? 'fourth' : 'hide'}  
+                    fifth={this.state.fifth ? 'fifth' : 'hide'}  
+                    sixth={this.state.sixth ? 'sixth' : 'hide'}  
+                    seventh={this.state.seventh ? 'seventh' : 'hide'}  
+                    eighth={this.state.eighth ? 'eighth' : 'hide'}  
+
+
                   />
                   <String
                     stringName='A' 
@@ -126,14 +265,74 @@ class App extends Component {
                     root={this.state.root}
                     mode={mode}
                     onOff={onOff}
+                    handleHighlight={this.handleHighlight}
+                    handleHighlight2={this.handleHighlight2}
+                    handleHighlight3={this.handleHighlight3}
+                    handleHighlight4={this.handleHighlight4}
+                    handleHighlight5={this.handleHighlight5}
+                    handleHighlight6={this.handleHighlight6}
+                    handleHighlight7={this.handleHighlight7}
+                    handleHighlight8={this.handleHighlight8}
+                    first={this.state.first ? 'first' : 'hide'}  
+                    second={this.state.second ? 'second' : 'hide'}  
+                    third={this.state.third ? 'third' : 'hide'}  
+                    fourth={this.state.fourth ? 'fourth' : 'hide'}  
+                    fifth={this.state.fifth ? 'fifth' : 'hide'}  
+                    sixth={this.state.sixth ? 'sixth' : 'hide'}  
+                    seventh={this.state.seventh ? 'seventh' : 'hide'}  
+                    eighth={this.state.eighth ? 'eighth' : 'hide'}  
+
+
                   />
                   <String
-                    stringName='E' 
-                    startNote={4}
+                    stringName='D' 
+                    startNote={2}
                     Chromatic={Chromatic}
                     root={this.state.root}
                     mode={mode}
                     onOff={onOff}
+                    handleHighlight={this.handleHighlight}
+                    handleHighlight2={this.handleHighlight2}
+                    handleHighlight3={this.handleHighlight3}
+                    handleHighlight4={this.handleHighlight4}
+                    handleHighlight5={this.handleHighlight5}
+                    handleHighlight6={this.handleHighlight6}
+                    handleHighlight7={this.handleHighlight7}
+                    handleHighlight8={this.handleHighlight8}
+                    first={this.state.first ? 'first' : 'hide'}  
+                    second={this.state.second ? 'second' : 'hide'}  
+                    third={this.state.third ? 'third' : 'hide'}  
+                    fourth={this.state.fourth ? 'fourth' : 'hide'}  
+                    fifth={this.state.fifth ? 'fifth' : 'hide'}  
+                    sixth={this.state.sixth ? 'sixth' : 'hide'}  
+                    seventh={this.state.seventh ? 'seventh' : 'hide'}  
+                    eighth={this.state.eighth ? 'eighth' : 'hide'}  
+
+
+                  />
+                  <String
+                    stringName='G' 
+                    startNote={7}
+                    Chromatic={Chromatic}
+                    root={this.state.root}
+                    mode={mode}
+                    onOff={onOff}
+                    handleHighlight={this.handleHighlight}
+                    handleHighlight2={this.handleHighlight2}
+                    handleHighlight3={this.handleHighlight3}
+                    handleHighlight4={this.handleHighlight4}
+                    handleHighlight5={this.handleHighlight5}
+                    handleHighlight6={this.handleHighlight6}
+                    handleHighlight7={this.handleHighlight7}
+                    handleHighlight8={this.handleHighlight8}
+                    first={this.state.first ? 'first' : 'hide'}  
+                    second={this.state.second ? 'second' : 'hide'}  
+                    third={this.state.third ? 'third' : 'hide'}  
+                    fourth={this.state.fourth ? 'fourth' : 'hide'}  
+                    fifth={this.state.fifth ? 'fifth' : 'hide'}  
+                    sixth={this.state.sixth ? 'sixth' : 'hide'}  
+                    seventh={this.state.seventh ? 'seventh' : 'hide'}  
+                    eighth={this.state.eighth ? 'eighth' : 'hide'}  
                   />
                   </div>
                   <div className='fretboardSides'/>                                <div className='numberContainer'>
--- src/components/String.js
+++ src/components/String.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {permute} from '../constants/helpers'
 
 const String = (props) => {
-  let {startNote, mode, Chromatic, root} = props
+  let {startNote, mode, Chromatic, root, 
+    handleHighlight, handleHighlight2, handleHighlight3, handleHighlight4, 
+    handleHighlight5, handleHighlight6, handleHighlight7, handleHighlight8,
+    first, second, third, fourth, fifth, sixth, seventh, eighth} = props
   let notes = mode[1].map(x => root[x])
   let string = permute(Chromatic, startNote)
   string = string.concat(string).concat(string[0])  
-
   return (
     <div className="stringContainer">
       { 
@@ -17,38 +19,35 @@ const String = (props) => {
           <div className='frets' key={j++}>
             {notes.indexOf(note) !== -1 ?
                   notes[0] === note ? 
-                    <div className='first'>{note}</div>
+                    <div onClick={handleHighlight} className={first}>{note}</div>
                     :
                   notes[1] === note ? 
-                    <div className='second'>{note}</div>
+                    <div onClick={handleHighlight2} className={second}>{note}</div>
                     :
                   notes[2] === note ? 
-                    <div className='third'>{note}</div>
+                    <div onClick={handleHighlight3} className={third}>{note}</div>
                     :
                   notes[3] === note ? 
-                    <div className='second'>{note}</div>
+                    <div onClick={handleHighlight4} className={fourth}>{note}</div>
                     :
                   notes[4] === note ? 
-                    <div className='third'>{note}</div>
+                    <div onClick={handleHighlight5} className={fifth}>{note}</div>
                     :
                   notes[5] === note ? 
-                    <div className='second'>{note}</div>
+                    <div onClick={handleHighlight6} className={sixth}>{note}</div>
                     :
                   notes[6] === note ? 
-                    <div className='third'>{note}</div>
+                    <div onClick={handleHighlight7} className={seventh}>{note}</div>
                     : 
                   notes[7] === note ? 
-                  <div className='second'>{note}</div>
+                    <div onClick={handleHighlight8} className={eighth}>{note}</div>
                   :
                   0
 
               :
               <div style={{background: ''}}>
-                
               </div>
-              
             }
-
           </div>
           )
           }
